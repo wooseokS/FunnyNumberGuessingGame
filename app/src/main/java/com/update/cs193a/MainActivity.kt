c92a6d13ac420cdb39eba0ebdd1371c16df189b5
wooseokS <song.wooseok48@gmail.com>
2019-07-15 16:22:05 +0900

    :cocktail: second commit

---
+++ app/src/main/java/com/update/cs193a/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.update.cs193a
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun chukck(view: View){
+    }
+
+}
